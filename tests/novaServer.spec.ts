0c131a25db88b09adb9628567cda5a53991c11bd
denshikov-vovan <denshikov_vovan@mail.ru>
2016-07-25 18:27:21 +0300

    [ #126853081 ] - added base tests

--- tests/novaServer.spec.ts
+++ tests/novaServer.spec.ts
@@ -16,130 +16,162 @@ interface ActionFunc {
     (this: ActionContext, inputs: any): Promise<any>;
 }
 
-let server: http.Server;
-let router: RouteController;
 let app: Application;
+let router: RouteController;
 let authenticator: Authenticator;
 let dao: Dao;
 let database: Database;
 let cache: Cache;
 let dispatcher: Dispatcher;
-let appOptions: AppConfig;
+
+let appConfig: AppConfig;
+let routeConfig: RouteConfig;
+let endpointConfig: EndpointConfig<any, any>;
 
 let adapter: AdapterFunc;
 let action: ActionFunc;
 let view: ViewBuilder<any>;
-let routeConfig: RouteConfig;
-let endpointConfig: EndpointConfig<any, any>;
+let fakeRequest: any;
 
-const authResult: any = { token: '1234567890' };
 const daoOptions: DaoOptions = { startTransaction: true };
 
 describe('NOVA-SERVER -> tests;', () => {
     beforeEach(() => {
-        server = http.createServer();
-        router = new RouteController();
-
-        authenticator = sinon.stub().returns(Promise.resolve(authResult));
         dao = new MockDao(daoOptions);
         database = { connect: sinon.stub().returns(Promise.resolve(dao)) };
-        dispatcher = { dispatch: sinon.stub().returns(Promise.resolve()) };
-
-        cache = {
-            get    : sinon.stub().returns(Promise.resolve()),
-            set    : sinon.stub(),
-            execute: sinon.stub().returns(Promise.resolve()),
-            clear  : sinon.stub()
-        };
-
-        sinon.spy(dao, 'release');
-
-        appOptions = {
-            name         : 'Test API Server',
-            version      : '0.0.1',
-            webServer    : server,
-            ioServer     : undefined,
-            database     : database,
-            cache        : cache,
-            dispatcher   : dispatcher,
-            authenticator: authenticator,
-            settings     : {}
-        };
+        dispatcher = { dispatch: sinon.stub() };
     });
 
-    describe('should create app without errors;', () => {
+    describe('creating app;', () => {
         beforeEach(() => {
-            app = createApp(appOptions);
+            appConfig = {
+                name    : 'Test API Server',
+                version : '0.0.1',
+                database: database
+            };
         });
 
         it('app object should be instance of Application', () => {
+            app = createApp(appConfig);
+
             expect(app).to.be.instanceof(Application);
         });
 
-    });
+        it('should return error if database was not provided', done => {
+            delete appConfig.database;
 
-    // describe('should run app server without any attached routes;', () => {
-    //     it('should return 404', done => {
-    //         app = createApp(appOptions);
-    //         try {
-    //             request(app.webServer)
-    //                 .get('/')
-    //                 .expect(404, done);
-    //
-    //         } catch (err) {
-    //             done(err);
-    //         }
-    //
-    //     });
-    // });
-
-    // describe('should return error if no actions was provided;', () => {
-    //     it('should return error', done => {
-    //         try {
-    //             app = createApp(appOptions);
-    //             router = new RouteController();
-    //             routeConfig = { get: undefined };
-    //             router.set('/', routeConfig);
-    //
-    //             app.register('/', router);
-    //
-    //             app = createApp(appOptions);
-    //             done('errror')
-    //         } catch (err) {
-    //             done(err);
-    //         }
-    //     });
-    // });
-
-    describe('GET \'\\\' route;', () => {
-        beforeEach(() => {
-            app = createApp(appOptions);
-
-            router = new RouteController();
-
-            action = sinon.stub().returns(Promise.resolve('result'));
-            view = sinon.stub().returns(Promise.resolve('result'));
-
-            endpointConfig = { action, response: view };
-            routeConfig = { get: endpointConfig };
-
-            router.set('/', routeConfig);
-
-            app.register('/', router);
+            try {
+                app = createApp(appConfig);
+                done('should return error');
+            } catch (err) {
+                expect(err).to.match(/Database is undefined/);
+                done();
+            }
         });
 
-        it('should return 200', done => {
+        it('should return error if server name was not provided', done => {
+            delete appConfig.name;
+
+            try {
+                app = createApp(appConfig);
+                done('should return error');
+            } catch (err) {
+                expect(err).to.match(/name is undefined/);
+                done();
+            }
+        });
+
+        it('should return error if server version was not provided', done => {
+            delete appConfig.version;
+
+            try {
+                app = createApp(appConfig);
+                done('should return error');
+            } catch (err) {
+                expect(err).to.match(/version is undefined/);
+                done();
+            }
+        });
+    });
+
+    describe('should run app server without any attached routes;', () => {
+        beforeEach(() => {
+            appConfig = {
+                name    : 'Test API Server',
+                version : '0.0.1',
+                database: database
+            };
+
+            app = createApp(appConfig);
+            fakeRequest = request(app.webServer).get('/');
+
+            app.on('error', () => undefined);
+        });
+
+        it('should return 404', done => {
+            fakeRequest
+                .expect(404, done);
+        });
+
+        it('should emmit error with \'does not exist\' message', done => {
+            fakeRequest
+                .end(done);
+
+            app.on('error', err => {
+                expect(err.message).to.equal('Endpoint for / does not exist');
+            });
+        });
+
+        it('should emmit error with 404 status', done => {
+            fakeRequest
+                .end(done);
+
+            app.on('error', err => {
+                expect(err.status).to.equal(404);
+            });
+        });
+    });
+
+    describe('should create GET \'\\\' API endpoint;', () => {
+        beforeEach(() => {
+            appConfig = {
+                name      : 'Test API Server',
+                version   : '0.0.1',
+                database  : database,
+                dispatcher: dispatcher
+            };
+
+            app = createApp(appConfig);
+            router = new RouteController();
+            endpointConfig = {
+                action: sinon.stub().returns(Promise.resolve())
+            };
+
+            routeConfig = {
+                get: endpointConfig
+            };
+        });
+
+        it('should return 204 if view was not provided', done => {
+            router.set('/', routeConfig);
+            app.register('/', router);
+            app.on('error', () => undefined);
+
             request(app.webServer)
                 .get('/')
-                .query({ author: 'test' })
-                .expect(200)
-                .end((err, res) => {
-                    if (err) {
-                        return done(err);
-                    }
+                .expect(204, done);
+        });
 
-                    done();
-                });
+        it('should return 200 if view was provided', done => {
+            endpointConfig.response = sinon.stub().returns(Promise.resolve());
+            routeConfig = { get: endpointConfig };
+            router.set('/', routeConfig);
+            app.register('/', router);
+            app.on('error', () => undefined);
+
+            request(app.webServer)
+                .get('/')
+                .expect(200, done);
         });
     });
 });
