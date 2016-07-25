dc39a48e72f5f0cff46d32ba0ec87369805599a0
denshikov-vovan <denshikov_vovan@mail.ru>
2016-07-25 20:11:09 +0300

    [ #126853081 ] - added tests

--- tests/novaServer.spec.ts
+++ tests/novaServer.spec.ts
@@ -1,36 +1,24 @@
 ///<reference path='../typings/tsd.d.ts'/>
-import * as http from 'http';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import * as request from 'supertest';
-import { ActionContext, Authenticator, Database, Dao, DaoOptions, Cache, Dispatcher } from 'nova-base';
+import { Authenticator, Database, Dao, DaoOptions, Dispatcher } from 'nova-base';
 import { createApp } from './../index';
 import { Application, AppConfig } from '../lib/Application';
-import { RouteController, RouteConfig, EndpointConfig, ViewBuilder } from '../lib/RouteController';
+import { RouteController, RouteConfig, EndpointConfig } from '../lib/RouteController';
 import { MockDao } from './mocks/Database';
-
-interface AdapterFunc {
-    (this: ActionContext, inputs: any, authInfo: any): Promise<any>;
-}
-interface ActionFunc {
-    (this: ActionContext, inputs: any): Promise<any>;
-}
 
 let app: Application;
 let router: RouteController;
 let authenticator: Authenticator;
 let dao: Dao;
 let database: Database;
-let cache: Cache;
 let dispatcher: Dispatcher;
 
 let appConfig: AppConfig;
 let routeConfig: RouteConfig;
 let endpointConfig: EndpointConfig<any, any>;
 
-let adapter: AdapterFunc;
-let action: ActionFunc;
-let view: ViewBuilder<any>;
 let fakeRequest: any;
 
 const daoOptions: DaoOptions = { startTransaction: true };
@@ -40,6 +28,7 @@ describe('NOVA-SERVER -> tests;', () => {
         dao = new MockDao(daoOptions);
         database = { connect: sinon.stub().returns(Promise.resolve(dao)) };
         dispatcher = { dispatch: sinon.stub() };
+        authenticator = sinon.stub().returns(Promise.resolve({ results: 'auth results' }));
     });
 
     describe('creating app;', () => {
@@ -144,7 +133,8 @@ describe('NOVA-SERVER -> tests;', () => {
             app = createApp(appConfig);
             router = new RouteController();
             endpointConfig = {
-                action: sinon.stub().returns(Promise.resolve())
+                action  : sinon.stub().returns(Promise.resolve({ results: 'action results' })),
+                response: sinon.stub().returns({ results: 'view results' })
             };
 
             routeConfig = {
@@ -152,26 +142,160 @@ describe('NOVA-SERVER -> tests;', () => {
             };
         });
 
-        it('should return 204 if view was not provided', done => {
-            router.set('/', routeConfig);
-            app.register('/', router);
-            app.on('error', () => undefined);
+        describe('without view function;', () => {
+            beforeEach(() => {
+                delete endpointConfig.response;
+                routeConfig = { get: endpointConfig };
+                router.set('/', routeConfig);
+                app.register('/', router);
+                app.on('error', () => undefined);
+                fakeRequest = request(app.webServer).get('/');
+            });
 
-            request(app.webServer)
-                .get('/')
-                .expect(204, done);
+            it('should return status 204', done => {
+                fakeRequest
+                    .expect(204, done);
+            });
+
+            it('should return empty body', done => {
+                fakeRequest
+                    .expect(204)
+                    .end((err, res) => {
+                        if (err) {
+                            return done(err);
+                        }
+
+                        expect(res.body).to.be.empty;
+                        done();
+                    });
+            });
         });
 
-        it('should return 200 if view was provided', done => {
-            endpointConfig.response = sinon.stub().returns(Promise.resolve());
-            routeConfig = { get: endpointConfig };
+        describe('with view function;', () => {
+            beforeEach(() => {
+                router.set('/', routeConfig);
+                app.register('/', router);
+                app.on('error', () => undefined);
+                fakeRequest = request(app.webServer).get('/');
+            });
+
+            it('should return status 200', done => {
+                fakeRequest
+                    .expect(200, done);
+            });
+
+            it('should return view result in body', done => {
+                fakeRequest
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) {
+                            return done(err);
+                        }
+
+                        expect(res.body).to.not.be.empty;
+                        expect(res.body).to.deep.equal({ results: 'view results' });
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('all functions should be called with right arguments;', () => {
+        beforeEach(() => {
+            appConfig = {
+                name         : 'Test API Server',
+                version      : '0.0.1',
+                database     : database,
+                dispatcher   : dispatcher,
+                authenticator: authenticator
+            };
+            endpointConfig = {
+                action : sinon.stub().returns(Promise.resolve({ results: 'action results' })),
+                adapter: sinon.stub().returns(Promise.resolve({ results: 'adapter results' }))
+            };
+            routeConfig = {
+                get: endpointConfig
+            };
+
+            app = createApp(appConfig);
+            router = new RouteController();
             router.set('/', routeConfig);
             app.register('/', router);
             app.on('error', () => undefined);
+        });
 
-            request(app.webServer)
-                .get('/')
-                .expect(200, done);
+        describe('when auth token was not provided;', () => {
+            beforeEach(done => {
+                request(app.webServer)
+                    .get('/')
+                    .expect(204)
+                    .end(done);
+            });
+
+            it('authenticator should not be called', () => {
+                expect((authenticator as any).called).to.be.false;
+            });
+
+            it('action should be called once', () => {
+                expect((endpointConfig.action as any).calledOnce).to.be.true;
+            });
+        });
+
+        describe('when auth token was provided;', () => {
+            beforeEach(done => {
+                request(app.webServer)
+                    .get('/')
+                    .set('Authorization', 'token awdfasdfasdasdfa')
+                    .expect(204, done);
+            });
+
+            it('authenticator should be called once', () => {
+                expect((authenticator as any).calledOnce).to.be.true;
+            });
+
+            it('authenticator should be called with right arguments', () => {
+                expect((authenticator as any).firstCall.calledWithExactly({
+                    scheme     : 'token',
+                    credentials: 'awdfasdfasdasdfa'
+                }, undefined)).to.be.true;
+            });
+
+            it('adapter should be called once', () => {
+                expect((endpointConfig.adapter as any).calledOnce).to.be.true;
+            });
+
+            it('adapter should be called with right arguments', () => {
+                expect((endpointConfig.adapter as any).firstCall.calledWithExactly({}, { results: 'auth results' })).to.be.true;
+            });
+        });
+
+        describe('when query was provided;', () => {
+            beforeEach(done => {
+                request(app.webServer)
+                    .get('/')
+                    .query({ some: 'data' })
+                    .set('Authorization', 'token awdfasdfasdasdfa')
+                    .expect(204, done);
+            });
+
+            it('authenticator should be called once', () => {
+                expect((authenticator as any).calledOnce).to.be.true;
+            });
+
+            it('authenticator should be called with right arguments', () => {
+                expect((authenticator as any).firstCall.calledWithExactly({
+                    scheme     : 'token',
+                    credentials: 'awdfasdfasdasdfa'
+                }, undefined)).to.be.true;
+            });
+
+            it('adapter should be called once', () => {
+                expect((endpointConfig.adapter as any).calledOnce).to.be.true;
+            });
+
+            it('adapter should be called with right arguments', () => {
+                expect((endpointConfig.adapter as any).firstCall.calledWithExactly({ some: 'data'}, { results: 'auth results' })).to.be.true;
+            });
         });
     });
 });
